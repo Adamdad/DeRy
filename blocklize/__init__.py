5b11176263eae0766ab0c77d63ec278563afd8e7
yifan.zhang1222@BYTEDANCE.COM <xyang@bytedance.com>
2022-11-23 17:00:02 +0800

    aa

--- blocklize/__init__.py
+++ blocklize/__init__.py
@@ -1,2 +1,2 @@
-from .block_meta import (MODEL2MODULES, MODEL_BLOCKS, MODEL_INOUT_SHAPE,
+from .block_meta import (MODEL_BLOCKS, MODEL_INOUT_SHAPE,
                          MODEL_PRINT, MODEL_STATS, MODEL_ZOO)
