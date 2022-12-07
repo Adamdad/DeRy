e05261afcb51dff6b16d6fc4efbfda17b2a55c58
adamdad <xyang@bytedance.com>
2022-12-07 16:59:35 +0800

    @BACKBONES.register_module()

--- mmcls_addon/models/__init__.py
+++ mmcls_addon/models/__init__.py
@@ -4,3 +4,5 @@ from .builder import (BACKBONES, CLASSIFIERS, HEADS, LOSSES, NECKS,
                       build_backbone, build_classifier, build_head, build_loss,
                       build_neck)
 
+print(BACKBONES)
+
